6eac6c3cf325184cfcafeab6901a09511e4d9aed
josephquang97 <=>
2022-01-15 09:02:58 +0000

    update v2

--- __main__.py
+++ __main__.py
@@ -1,27 +1,45 @@
-from getpass import getpass
 from schema import SQLite, Memrise
 import logging
+from getpass import getpass
 
-PATH = "C:\\Users\\JosephQuang\\Desktop\\ELSA\\"
-FILE = "Innovation.db"
-
+FILE = "course.db"
 
 if __name__ == "__main__":
     # Enter username and password here
-    __username__ = input('Enter username: ')
-    __password__ = getpass('Enter password: ')
+    __username__ = input("Enter username: ")
+    __password__ = getpass("Enter password: ")
+
+    # Sign in Memrise
     user = Memrise()
     user.login(__username__, __password__)
+
+    # Choose the course
     course = user.select_course()
-    # df = pd.read_csv('vocabulary.csv',encoding='utf-16',sep='\t', header=None)
-    db = SQLite(f"{PATH}{FILE}")
-    df_topic = db.select_local_topic()  # id | topic
+
+    # Read the database find the new levels
+    db = SQLite(f"./course/{FILE}")
+    
+    # Select the local topics
+    df_topic = db.select_local_topic()
+    
+    # Retrieval all topics to convert to bulk
+    # And then connect to MEMRISE add bulk to new level for each topic
     for idx in range(df_topic.shape[0]):
-        bulk = db.topic_to_bulk(int(df_topic[0][idx]))
-        status = course.add_level_with_bulk(df_topic[1][idx], bulk, "tab")
+        sep = "\t"
+        # Get topic id & name
+        topic_id = int(df_topic[0][idx])
+        topic_name = df_topic[1][idx]
+        # Streaming data
+        bulk = db.topic_to_bulk(topic_id, sep)
+        status = course.add_level_with_bulk(topic_name, bulk, sep)
+        # Validate the status
         if status:
-            logging.warning(f"Successful to add level {df_topic[0][idx]}")
+            logging.Logger(f"Successful to add level {topic_id}")
             db.switch_status(str(df_topic[0][idx]))
         else:
-            logging.warning(f"Failed to add level {df_topic[0][idx]}")
+            logging.warning(f"Failed to add level {topic_id}")
+    # Close the database
     db.close()
+
+    # Update the audio for each levels
+    course.update_audio('en', output=True)
